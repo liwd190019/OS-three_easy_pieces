bef3f43574817db36a24acc1764c9b54fa33812a
wenda_li <wdli@umich.edu>
2023-11-19 13:42:13 -0500

    begin shell

---
+++ wish/wish.c
@@ -0,0 +1,16 @@
+#include "wish.h"
+#include <ctype.h>  // isspace
+#include <regex.h>  // regcomp, regexec, regfree
+#include <stdio.h>  // fopen, fclose, fileno, getline, feof
+#include <stdlib.h> // exit
+#include <sys/types.h>
+#include <sys/wait.h> // waitpid
+
+
+int main(int argc, char *argv[]) {
+  // YOUR CODE HERE
+
+  
+  return 0;
+
+}
